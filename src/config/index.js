dadd32079967b84843aa1999baf66d59266a58a7
Joseph Salen <jsalen.91@gmail.com>
2021-11-23 19:34:08 -0400

    create configuration main file

---
+++ src/config/index.js
@@ -0,0 +1,13 @@
+require('dotenv').config()
+
+const config = {
+  port: process.env.PORT || 3005,
+  dbUser: process.env.DB_USER,
+  dbPassword: process.env.DB_PASSWORD,
+  dbHost: process.env.DB_HOST,
+  dbName: process.env.DB_NAME,
+  dbPort: process.env.DB_PORT,
+  dbUrl: process.env.DATABASE_URL,
+}
+
+module.exports = { config }

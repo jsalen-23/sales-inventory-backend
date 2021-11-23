0ee876cf802962f57fdd45a7b162569e3b76d5bb
Joseph Salen <jsalen.91@gmail.com>
2021-11-23 19:39:48 -0400

    middlewares, routes and server ready

---
+++ src/index.js
@@ -0,0 +1,44 @@
+const process = require('process')
+const express = require('express')
+const passport = require('passport')
+const cors = require('cors')
+const routerAPI = require('./routes')
+const {
+  logErrors,
+  queryErrorHandler,
+  boomErrorHandler,
+  errorHandler,
+} = require('./middlewares/error.handler')
+
+const app = express()
+const PORT = process.env.PORT
+
+// Middlewares
+app.use(express.json())
+app.use(passport.initialize())
+
+const whiteList = ['http://localhost:5500']
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (whiteList.includes(origin) || !origin) {
+      callback(null, true)
+    } else {
+      callback(new Error('Access Denied.'))
+    }
+  },
+}
+app.use(cors(corsOptions))
+
+// Routes
+routerAPI(app)
+
+// Error Middlewares
+app.use(logErrors)
+app.use(queryErrorHandler)
+app.use(boomErrorHandler)
+app.use(errorHandler)
+
+// Server
+app.listen(PORT, () =>
+  console.log(`Server running on http://localhost:${PORT}`)
+)

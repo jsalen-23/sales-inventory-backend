8c55a1235775eed0d06f2ac134238180d9b11a40
Joseph Salen <jsalen.91@gmail.com>
2021-11-24 14:00:35 -0400

    fix migrations bug

--- src/db/models/index.js
+++ src/db/models/index.js
@@ -6,21 +6,21 @@ const { Customer, CustomerSchema } = require('./customer.model')
 const { Seller, SellerSchema } = require('./seller.model')
 const { User, UserSchema } = require('./user.model')
 
-function setupModels (sequelize) {
+function setupModels(sequelize) {
   Category.init(CategorySchema, Category.config(sequelize))
-  Product.init(ProductSchema, Product.config(sequelize))
-  OrderProduct.init(OrderProductSchema, OrderProduct.config(sequelize))
-  Order.init(OrderSchema, Order.config(sequelize))
   Customer.init(CustomerSchema, Customer.config(sequelize))
-  Seller.init(SellerSchema, Seller.config(sequelize))
   User.init(UserSchema, User.config(sequelize))
+  Seller.init(SellerSchema, Seller.config(sequelize))
+  OrderProduct.init(OrderProductSchema, OrderProduct.config(sequelize))
+  Product.init(ProductSchema, Product.config(sequelize))
+  Order.init(OrderSchema, Order.config(sequelize))
 
   Category.associate(sequelize.models)
+  Customer.associate(sequelize.models)
+  User.associate(sequelize.models)
+  Seller.associate(sequelize.models)
   Product.associate(sequelize.models)
   Order.associate(sequelize.models)
-  Customer.associate(sequelize.models)
-  Seller.associate(sequelize.models)
-  User.associate(sequelize.models)
 }
 
 module.exports = setupModels

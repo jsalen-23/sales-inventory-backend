b1e84271b6fb3367e37eca15c569163b545a2ae1
Joseph Salen <jsalen.91@gmail.com>
2021-11-23 22:32:10 -0400

    create boom validator middleware

---
+++ src/middlewares/validator.handler.js
@@ -0,0 +1,16 @@
+const boom = require('@hapi/boom')
+
+function validatorHandler(schema, prop) {
+  return (req, res, next) => {
+    const data = req[prop]
+    const { error } = schema.validate(data, { abortEarly: false })
+
+    if (error) {
+      next(boom.badRequest(error))
+    }
+
+    next()
+  }
+}
+
+module.exports = validatorHandler

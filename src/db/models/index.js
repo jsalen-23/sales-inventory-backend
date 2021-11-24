f9a858657ff452cc1d52b7432a38fc84ba55e7f1
Joseph Salen <jsalen.91@gmail.com>
2021-11-24 11:39:34 -0400

    add category and product sequelize configuration

--- src/db/models/index.js
+++ src/db/models/index.js
@@ -1,3 +1,12 @@
-function setupModels (sequelize) {}
+const { Category, CategorySchema } = require('./category.model')
+const { Product, ProductSchema } = require('./product.model')
+
+function setupModels(sequelize) {
+  Category.init(CategorySchema, Category.config(sequelize))
+  Product.init(ProductSchema, Product.config(sequelize))
+
+  Category.associate(sequelize.models)
+  Product.associate(sequelize.models)
+}
 
 module.exports = setupModels

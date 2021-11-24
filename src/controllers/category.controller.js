2acb90f34e2bed8976e6075abaa96ccacf9b5265
Joseph Salen <jsalen.91@gmail.com>
2021-11-24 17:48:45 -0400

    resolve category routing and service

--- src/controllers/category.controller.js
+++ src/controllers/category.controller.js
@@ -1,40 +1,58 @@
+const CategoryService = require('../services/category.service')
 const categoryController = {}
 
-categoryController.getCategories = (req, res, next) => {
+const service = new CategoryService()
+
+categoryController.getCategories = async (req, res, next) => {
   try {
-    res.json({ message: 'Get Categories' })
+    const categories = await service.find()
+
+    return res.status(200).json(categories)
   } catch (error) {
     next(error)
   }
 }
 
-categoryController.createCategory = (req, res, next) => {
+categoryController.createCategory = async (req, res, next) => {
   try {
-    res.json({ message: 'Category created' })
+    const body = req.body
+    const newCategory = await service.create(body)
+
+    return res.status(201).json(newCategory)
   } catch (error) {
     next(error)
   }
 }
 
-categoryController.getSingleCategory = (req, res, next) => {
+categoryController.getSingleCategory = async (req, res, next) => {
   try {
-    res.json({ message: 'Single Category' })
+    const { id } = req.params
+    const category = await service.findOne(id)
+
+    return res.status(200).json(category)
   } catch (error) {
     next(error)
   }
 }
 
-categoryController.updateCategory = (req, res, next) => {
+categoryController.updateCategory = async (req, res, next) => {
   try {
-    res.json({ message: 'Update Category' })
+    const { id } = req.params
+    const body = req.body
+    const updatedCategory = await service.update(id, body)
+
+    return res.status(200).json(updatedCategory)
   } catch (error) {
     next(error)
   }
 }
 
-categoryController.deleteCategory = (req, res, next) => {
+categoryController.deleteCategory = async (req, res, next) => {
   try {
-    res.json({ message: 'Delete Category' })
+    const { id } = req.params
+    const deletedCategory = await service.delete(id)
+
+    return res.status(200).json(deletedCategory)
   } catch (error) {
     next(error)
   }

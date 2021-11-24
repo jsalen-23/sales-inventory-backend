683cb3bf58bcecf098b259fc19694b20f8b5ab10
Joseph Salen <jsalen.91@gmail.com>
2021-11-24 13:31:05 -0400

    update with every model and association

--- src/db/models/index.js
+++ src/db/models/index.js
@@ -1,12 +1,26 @@
 const { Category, CategorySchema } = require('./category.model')
 const { Product, ProductSchema } = require('./product.model')
+const { OrderProduct, OrderProductSchema } = require('./order-product.model')
+const { Order, OrderSchema } = require('./order.model')
+const { Customer, CustomerSchema } = require('./customer.model')
+const { Seller, SellerSchema } = require('./seller.model')
+const { User, UserSchema } = require('./user.model')
 
-function setupModels(sequelize) {
+function setupModels (sequelize) {
   Category.init(CategorySchema, Category.config(sequelize))
   Product.init(ProductSchema, Product.config(sequelize))
+  OrderProduct.init(OrderProductSchema, OrderProduct.config(sequelize))
+  Order.init(OrderSchema, Order.config(sequelize))
+  Customer.init(CustomerSchema, Customer.config(sequelize))
+  Seller.init(SellerSchema, Seller.config(sequelize))
+  User.init(UserSchema, User.config(sequelize))
 
   Category.associate(sequelize.models)
   Product.associate(sequelize.models)
+  Order.associate(sequelize.models)
+  Customer.associate(sequelize.models)
+  Seller.associate(sequelize.models)
+  User.associate(sequelize.models)
 }
 
 module.exports = setupModels

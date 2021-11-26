a08fab8ced7eef260603194e1bd169a6268980e3
Joseph Salen <jsalen.91@gmail.com>
2021-11-26 14:10:58 -0400

    add jwt secret

--- src/config/index.js
+++ src/config/index.js
@@ -9,7 +9,8 @@ const config = {
   dbHost: process.env.DB_HOST,
   dbName: process.env.DB_NAME,
   dbPort: process.env.DB_PORT,
-  dbUrl: process.env.DATABASE_URL
+  dbUrl: process.env.DATABASE_URL,
+  jwtSecret: process.env.JWT_SECRET,
 }
 
 module.exports = { config }

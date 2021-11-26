a9de65ada7b29eec0eca4b4375fbe99f70565ce4
Joseph Salen <jsalen.91@gmail.com>
2021-11-26 14:14:43 -0400

    create check role middleware

---
+++ src/middlewares/auth.handler.js
@@ -0,0 +1,15 @@
+const boom = require('@hapi/boom')
+
+function checkRole(roles) {
+  return (req, res, next) => {
+    const { role } = req.user
+
+    if (roles.includes(role)) {
+      next()
+    } else {
+      next(boom.unauthorized())
+    }
+  }
+}
+
+module.exports = { checkRole }
